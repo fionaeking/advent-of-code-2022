3264df6264c3e190d26b5b66e7b5693afa749229
fionaeking <fionaeking@gmail.com>
2022-12-05 00:03:31 +0000

    Add first part of Day 3

---
+++ DayThree/DayThree/InvertCharExtension.cs
@@ -0,0 +1,10 @@
+﻿public static class InvertCharExtension
+{
+    public static char Invert(this char c)
+    {
+        if (!char.IsLetter(c))
+            return c;
+
+        return char.IsUpper(c) ? char.ToLower(c) : char.ToUpper(c);
+    }
+}
---
+++ DayThree/DayThree/Program.cs
@@ -0,0 +1,17 @@
+﻿var sum = 0;
+foreach (var line in File.ReadAllLines("PuzzleInput.txt"))
+{
+    sum += ConvertCharToInt(
+              line[..(line.Length / 2)].Distinct()
+              .Concat(line[(line.Length / 2)..].Distinct())
+              .GroupBy(x => x)
+              .OrderByDescending(x => x.Count())
+              .First().Key.Invert()
+            );
+}
+Console.WriteLine($"Part one answer: {sum}");
+
+static int ConvertCharToInt(char c)
+{
+    return (char.IsLower(c)) ? (int)c - 70 : (int)c - 64;
+}
