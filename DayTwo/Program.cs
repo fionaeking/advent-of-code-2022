a68e3f573ea786944b75b44ddfe78436fd119b0e
fionaeking <fionaeking@gmail.com>
2022-12-05 00:38:32 +0000

    Use Model for Day2

--- DayTwo/Program.cs
+++ DayTwo/Program.cs
@@ -1,11 +1,36 @@
-﻿// TODO - very messy and inefficient!
+﻿using DayTwo;
 
-var rpsMapping = new Dictionary<RPS, int>()
+const List<RPSModel> models = new List<RPSModel>()
 {
-    { RPS.X, 1},
-    { RPS.Y, 2},
-    { RPS.Z, 3},
+    new RPSModel()
+    {
+        Type = RPSType.Rock,
+        LosesTo = RPSType.Paper,
+        WinsAgainst = RPSType.Scissors,
+        Score = 1,
+        MapsFromOpponent = InputOptions.A,
+        MapsFromYou = InputOptions.X
+    },
+    new RPSModel()
+    {
+        Type = RPSType.Paper,
+        LosesTo = RPSType.Scissors,
+        WinsAgainst = RPSType.Rock,
+        Score = 2,
+        MapsFromOpponent =InputOptions.B,
+        MapsFromYou = InputOptions.Y
+    },
+    new RPSModel()
+    {
+        Type = RPSType.Scissors,
+        LosesTo = RPSType.Rock,
+        WinsAgainst = RPSType.Paper,
+        Score = 3,
+        MapsFromOpponent = InputOptions.C,
+        MapsFromYou = InputOptions.Z
+    },
 };
+
 
 const int winScore = 6;
 const int drawScore = 3;
@@ -20,116 +45,54 @@ foreach (var line in File.ReadAllLines("PuzzleInput.txt"))
         Console.WriteLine($"Unexpected line: {line}");
         continue;
     }
-    var opponent = (RPS)Enum.Parse(typeof(RPS), split[0]);
-    var you = (RPS)Enum.Parse(typeof(RPS), split[1]);
+    var opponent = models.First(x => x.MapsFromOpponent == (InputOptions)Enum.Parse(typeof(InputOptions), split[0]));
+    var you = models.First(x => x.MapsFromYou == (InputOptions)Enum.Parse(typeof(InputOptions), split[1]));
 
     // Rock defeats Scissors, Scissors defeats Paper, and Paper defeats Rock. 
     partOneScore += PartOne(opponent, you);
     partTwoScore += PartTwo(opponent, you);
 }
 
-Console.WriteLine($"Final score: {partOneScore}");
-Console.WriteLine($"Final score: {partTwoScore}");
+Console.WriteLine($"Final score for part 1: {partOneScore}");
+Console.WriteLine($"Final score for part 2: {partTwoScore}");
 
 
-int PartOne(RPS opponent, RPS you)
+int PartOne(RPSModel opponent, RPSModel you)
 {
-    var score = rpsMapping[you];
-    if (opponent == RPS.A)
+    var score = you.Score;
+    if (opponent.LosesTo == you.Type)
     {
-        if (you == RPS.Y)
-        {
-            score += winScore;
-        }
-        else if (you == RPS.X)
-        {
-            score += drawScore;
-        }
+        score += winScore;
     }
-    else if (opponent == RPS.B)
+    else if (opponent.Type == you.Type)
     {
-        if (you == RPS.Z)
-        {
-            score += winScore;
-        }
-        else if (you == RPS.Y)
-        {
-            score += drawScore;
-        }
-    }
-    else
-    {
-        if (you == RPS.X)
-        {
-            score += winScore;
-        }
-        else if (you == RPS.Z)
-        {
-            score += drawScore;
-        }
+        score += drawScore;
     }
     return score;
 }
 
-int PartTwo(RPS opponent, RPS you)
+int PartTwo(RPSModel opponent, RPSModel you)
 {
     var score = 0;
-    if (you == RPS.X)
+    if (you.MapsFromYou == InputOptions.X)
     {
-        //Lose
-        if (opponent == RPS.A)
-        {
-            // Rock - to lose, need to use scissors
-            score += rpsMapping[RPS.Z];
-        }
-        else if (opponent == RPS.B)
-        {
-            score += rpsMapping[RPS.X];
-        }
-        else
-        {
-            score += rpsMapping[RPS.Y];
-        }
+        score += models.First(x => x.Type == opponent.WinsAgainst).Score;
     }
-    else if (you == RPS.Y)
+    else if (you.MapsFromYou == InputOptions.Y)
     {
-        //Draw
         score += drawScore;
-        if (opponent == RPS.A)
-        {
-            score += rpsMapping[RPS.X];
-        }
-        else if (opponent == RPS.B)
-        {
-            score += rpsMapping[RPS.Y];
-        }
-        else
-        {
-            score += rpsMapping[RPS.Z];
-        }
+        score += models.First(x => x.Type == opponent.Type).Score;
     }
     else
     {
-        //Win
         score += winScore;
-        if (opponent == RPS.A)
-        {
-            score += rpsMapping[RPS.Y];
-        }
-        else if (opponent == RPS.B)
-        {
-            score += rpsMapping[RPS.Z];
-        }
-        else
-        {
-            score += rpsMapping[RPS.X];
-        }
+        score += models.First(x => x.Type == opponent.LosesTo).Score;
     }
     return score;
 }
 
 
-enum RPS
+enum InputOptions
 {
     A,
     B,
@@ -137,4 +100,11 @@ enum RPS
     X,
     Y,
     Z
+}
+
+enum RPSType
+{
+    Rock,
+    Paper,
+    Scissors
 }
---
+++ DayTwo/RPSModel.cs
@@ -0,0 +1,12 @@
+﻿namespace DayTwo
+{
+    internal class RPSModel
+    {
+        public RPSType Type;
+        public RPSType LosesTo;
+        public RPSType WinsAgainst;
+        public int Score;
+        public InputOptions MapsFromOpponent;
+        public InputOptions MapsFromYou;
+    }
+}
