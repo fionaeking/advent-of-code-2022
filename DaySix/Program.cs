c3a921d94c2cf5967c444ae74353762f891e8509
fionaeking <fionaeking@gmail.com>
2022-12-07 21:50:35 +0000

    Add Day 6 Part 1

---
+++ DaySix/Program.cs
@@ -0,0 +1,17 @@
+﻿var inputString = File.ReadLines("PuzzleInput.txt").First();
+Console.WriteLine(GetEndOfMarkerIndex(inputString.ToList()));
+
+
+int GetEndOfMarkerIndex(List<char> input)
+{
+    var count = 0;
+    while (input.Count > 3 + count)
+    {
+        if (input.Skip(count).Take(4).Distinct().Count() == 4)
+        {
+            return count + 4;
+        }
+        count++;
+    }
+    throw new Exception("No markers found in string");
+}
