a1544346923ee85dd1bca28892c9dcc53912ca36
fionaeking <fionaeking@gmail.com>
2022-12-15 15:16:30 +0000

    Add Day 10

--- DayTen/Program.cs
+++ DayTen/Program.cs
@@ -15,16 +15,39 @@
 var cycle = 0;
 var registerValue = 1;
 var nextCycleReading = 20;
+var nextNewline = 40;
 var sum = 0;
-foreach (var i in File.ReadAllLines("PuzzleInput.txt")
+
+const int crt = 40;
+
+var answer = "";
+
+foreach (var instruction in File.ReadAllLines("PuzzleInput.txt")
     .Select(x => new InstructionValue()
     {
         Name = (CpuInstruction)Enum.Parse(typeof(CpuInstruction), x.Split(" ")[0]),
         Value = (x.Split(" ").Length > 1) ? int.Parse(x.Split(" ")[1]) : 0
     }))
 {
-    var time = types.First(x => x.Instruction == i.Name).Time;
-    cycle += time;
+    for (int i = 0; i < types.First(x => x.Instruction == instruction.Name).Time; i++)
+    {
+        if (registerValue - 1 <= (cycle % crt) && (cycle % crt) <= registerValue + 1)
+        {
+            answer += "#";
+        }
+        else
+        {
+            answer += ".";
+        }
+        cycle++;
+
+        if (cycle == nextNewline)
+        {
+            answer += "\n";
+            nextNewline += crt;
+        }
+    }
+
 
     if (cycle >= nextCycleReading)
     {
@@ -32,10 +55,12 @@ foreach (var i in File.ReadAllLines("PuzzleInput.txt")
         nextCycleReading += 40;
     }
 
-    if (i.Name == CpuInstruction.addx)
+    if (instruction.Name == CpuInstruction.addx)
     {
-        registerValue += i.Value;
+        registerValue += instruction.Value;
     }
 
 }
 Console.WriteLine(sum);
+
+Console.WriteLine(answer);
