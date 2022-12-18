b8357e0d21a64965bb1c069b3e26983d06433817
fionaeking <fionaeking@gmail.com>
2022-12-18 15:54:49 +0000

    Add Day 13 Part 2

--- DayThirteen/Program.cs
+++ DayThirteen/Program.cs
@@ -1,83 +1,26 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using DayThirteen;
 
 var index = 0;
 var sum = 0;
-foreach (var pair in File.ReadAllText("PuzzleInput.txt").Split(new string[] { Environment.NewLine + Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries))
+var input = File.ReadAllText("PuzzleInput.txt");
+var partOne = input.Split(new string[] { Environment.NewLine + Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+var comparer = new SignalComparer();
+foreach (var pair in partOne)
 {
     index++;
-
-    if (CompareJTokenArrays(ParseInputString(pair.Split(Environment.NewLine)[0]),
-        ParseInputString(pair.Split(Environment.NewLine)[1])))
-    {
-        sum += index;
-    }
-
-}
-Console.WriteLine(sum);
-
-bool CompareJTokenArrays(JToken[] arrayA, JToken[] arrayB)
-{
-    for (int i = 0; i < arrayA.Length; i++)
-    {
-        if (i > arrayB.Length - 1)
-        {
-            return false;
-        }
-        var result = CompareTokens(arrayA[i], arrayB[i]);
-        if (result is not null) return (bool)result;
-    }
-    return arrayA.Length <= arrayB.Length;
+    var one = pair.Split(Environment.NewLine).ToList();
+    var two = pair.Split(Environment.NewLine).ToList();
+    two.Sort(comparer);
+    if (two.SequenceEqual(one)) sum += index;
 }
 
+Console.WriteLine($"Part one: {sum}");
 
-bool? CompareTokens(JToken a, JToken b)
-{
-    if (a.Type == JTokenType.Integer && b.Type == JTokenType.Integer)
-    {
-        if ((int)a == (int)b) return null;
-        else return (int)a < (int)b;
-    }
-    else if (a.Type == JTokenType.Array && b.Type == JTokenType.Array)
-    {
-        for (int i = 0; i < a.Count(); i++)
-        {
-            if (i >= (b as JArray).Count) return false;
-            var result = CompareTokens((a as JArray)[i], (b as JArray)[i]);
-            if (result is not null)
-            {
-                return result;
-            }
-        }
-        if ((a as JArray).Count < (b as JArray).Count) return true;
-        return null;
-    }
-    else if (a.Type == JTokenType.Array && b.Type == JTokenType.Integer)
-    {
-        if (!(a as JArray).Any()) return true;
-        for (int l = 0; l < a.Count(); l++)
-        {
-            var result = CompareTokens((a as JArray)[l], b);
-            if (result is not null) return result;
-        }
-        return null;
-    }
-    else if (a.Type == JTokenType.Integer && b.Type == JTokenType.Array)
-    {
-        if (!(b as JArray).Any()) return false;
-        for (int l = 0; l < b.Count(); l++)
-        {
-            var result = CompareTokens(a, (b as JArray)[l]);
-            if (result is not null) return result;
-        }
-        return null;
-    }
-    else
-    {
-        throw new Exception();
-    }
-}
+const string dividerOne = "[[2]]";
+const string dividerTwo = "[[6]]";
+var partTwo = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).Append(dividerOne).Append(dividerTwo).ToList();
+partTwo.Sort(comparer);
 
-JToken[] ParseInputString(string inputString)
-{
-    return JObject.Parse("{ \"Data\":" + inputString + "}").First.First.Children().ToArray();
-}
+Console.WriteLine($"Part two: {(partTwo.IndexOf(dividerOne) + 1) * (partTwo.IndexOf(dividerTwo) + 1)}");
+
+
---
+++ DayThirteen/SignalComparer.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace DayThirteen
+{
+    class SignalComparer : IComparer<string>
+    {
+        public int Compare(string x, string y)
+        {
+            var arrayA = ParseInputString(x);
+            var arrayB = ParseInputString(y);
+            for (int i = 0; i < arrayA.Length; i++)
+            {
+                if (i > arrayB.Length - 1)
+                {
+                    return 1;
+                }
+                var result = CompareTokens(arrayA[i], arrayB[i]);
+                if (result is not null) return result == true ? -1 : 1;
+            }
+            return (arrayA.Length <= arrayB.Length) ? -1 : 1;
+        }
+
+        static JToken[] ParseInputString(string inputString)
+        {
+            return JObject.Parse("{ \"Data\":" + inputString + "}").First.First.Children().ToArray();
+        }
+
+        bool? CompareTokens(JToken a, JToken b)
+        {
+            if (a.Type == JTokenType.Integer && b.Type == JTokenType.Integer)
+            {
+                if ((int)a == (int)b) return null;
+                else return (int)a < (int)b;
+            }
+            else if (a.Type == JTokenType.Array && b.Type == JTokenType.Array)
+            {
+                for (int i = 0; i < a.Count(); i++)
+                {
+                    if (i >= (b as JArray).Count) return false;
+                    var result = CompareTokens((a as JArray)[i], (b as JArray)[i]);
+                    if (result is not null)
+                    {
+                        return result;
+                    }
+                }
+                if ((a as JArray).Count < (b as JArray).Count) return true;
+                return null;
+            }
+            else if (a.Type == JTokenType.Array && b.Type == JTokenType.Integer)
+            {
+                if (!(a as JArray).Any()) return true;
+                var m = JToken.FromObject(new List<int>() { (int)b });
+                return CompareTokens((a as JArray), m);
+            }
+            else if (a.Type == JTokenType.Integer && b.Type == JTokenType.Array)
+            {
+                if (!(b as JArray).Any()) return false;
+                var m = JToken.FromObject(new List<int>() { (int)a });
+                return CompareTokens(m, (b as JArray));
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
+
+    }
+}
+
