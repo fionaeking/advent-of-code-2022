90ad62734e63ccde1bdce0e11d2081a6ed137ebe
fionaeking <fionaeking@gmail.com>
2022-12-28 19:16:37 +0000

    Add Day 18 Part 1

---
+++ Day18/Program.cs
@@ -0,0 +1,24 @@
+﻿var input = File.ReadAllLines("PuzzleInput.txt").Select(x => x.Split(',').Select(y => int.Parse(y)).ToArray());
+var sideCount = 0;
+
+
+foreach (var item in input)
+{
+    var sides = 6;
+    foreach (var otherItem in input.Where(x => x != item))
+    {
+        if (Compare(item, otherItem))
+        {
+            sides--;
+        }
+    }
+    sideCount += sides;
+}
+
+Console.Write(sideCount);
+
+static bool Compare(int[] a, int[] b)
+{
+    return Math.Abs(a[0] - b[0]) + Math.Abs(a[1] - b[1]) + Math.Abs(a[2] - b[2]) == 1;
+
+}
